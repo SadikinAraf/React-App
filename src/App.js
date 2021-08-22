243b73212fec179d033c47ead59ac72b1c167916
Sadikin Araf <86142948+SadikinAraf@users.noreply.github.com>
2021-08-22 06:53:07 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
-
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+import React from 'react';
+import Card1 from './component/card';
+import Card2 from './component/card2'
+const desc = "Lorem ipsum dolor sit consectetur adipisicing elit. Deleniti eius rem in tempora accusamus dolorem sunt adipisci placeat quod cumque?"
+function App(){
+return(
+  <div>
+    <div className="cardHolder1">
+    <Card1 classCss="one com" name = "Card 1" desc={desc} btext="Click me"></Card1>
+    <Card1 classCss="two com" name = "Card 2" desc={desc} btext="Click me"></Card1>
+    <Card1 classCss="three com" name = "Card 3" desc={desc} btext="Click me" ></Card1>
+    </div>
+    
+  <Card2 name="Sadikin"></Card2>
+  </div>
+)
 }
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
